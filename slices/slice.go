ab38239dc7f69cfe186b929ff35e0e4c97fa3c24
yeqiang <ye0917@firestorm-sea.com>
2021-12-02 23:07:05 +0800

    切片

--- slices/slice.go
+++ slices/slice.go
@@ -1,5 +1,28 @@
 package main
 
+import "fmt"
+
 func main() {
-	$END$
+	//引用类型
+	var array = []int{2,3,4,5}
+	slice := array[1:3]
+	fmt.Println(slice)
+	fmt.Println(len(slice))
+	fmt.Println(cap(slice))
+
+	var ss = make([]int, 2, 10)
+	ss[0] = 2
+	ss[1] = 5
+	//ss[3] = 5 错误，数组下标越界
+	ss = append(ss, 4,6,10,12,16) //追加多个元素
+	fmt.Println(ss)
+	fmt.Println(len(ss))
+	fmt.Println(cap(ss))
+	fmt.Println(ss== nil)
+
+	var pp []int
+	pp = append(pp, 23,332,4421) //追加切片
+	pp = append(pp, pp...)
+	pp = append(pp, ss...)
+	fmt.Println(pp)
 }
--- slices/slice1.go
+++ slices/slice1.go
@@ -1,12 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
-	var a = []int{1,2,3, 4, 5}
-	var b = make([]int, 5, 10)
+	var a = []int{1,2,3, 4, 5, 6}
+	var b = make([]int, 6, 10)
+	a = append(a, 6, 7,8)
 	fmt.Println(a)
-	append(a, 6, 7,8)
-	copy(b, a)
+	copy(b, a) //复制
 	fmt.Println(b)
+
+	var c = []int{1,2,3, 10, 9,4,5,6}
+	c = append(c[:2], c[3:]...) //删除
+	fmt.Println(c)
+	sort.Ints(c)
+	fmt.Println(c)
 }
