0c6afdd74fe564a93220217adbab2d781e93c656
yeqiang <ye0917@firestorm-sea.com>
2021-12-02 23:07:22 +0800

    散列

--- maps/map.go
+++ maps/map.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
-	$END$
+	var m = make(map[string]int , 8)
+	m["a"] = 1
+	m["b"] = 2
+	m["c"] = 3
+	fmt.Println(m)
+
+	v, ok := m["c"]
+	fmt.Println(v, ok)
+	v1, ok1 := m["b"]
+	fmt.Println(v1, ok1)
+	delete(m, "b")
+	for key, val := range m{
+		fmt.Println(key, val)
+	}
 }
