fe63f3b0b8f759f0ebc96017a3481184735f46e9
yeqiang <qiang.ye@patpat.com>
2022-01-18 19:42:33 +0800

    json操作

--- json/json.go
+++ json/json.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct {
+	Name string `json:"myName"` //字段要大写
+	Age int64 `json:"ageInfo"`
+	Sex string`json:"common_sex"`
+}
+
 func main() {
-	$END$
+
+	p := Person{
+		Name: "mike",
+		Age: 28,
+		Sex: "male",
+	}
+	bytes, err := json.Marshal(p)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(bytes))
+
+	var p2 Person
+	err = json.Unmarshal(bytes, &p2)
+	if err != nil {
+		fmt.Errorf("%s", err)
+		return
+	}
+	fmt.Println(p2)
 }
